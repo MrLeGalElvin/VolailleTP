fc6b06749030a9e98c3ea2f2447d6b0ac672cd6a
Florian <flb090803@gmail.com>
2023-12-21 15:53:17 +0100

    Javadoc + Terminal

--- volaille/src/Canard.java
+++ volaille/src/Canard.java
@@ -3,12 +3,22 @@ class Canard extends Volaille{
     Canard(double p, int i){
         super(p,i);
     }
-    static void changePoidsCanard(double x){
+    /**
+     * Change le poids du canard pour l'abattage
+     * @param x : le poids du canard
+     */
+    static void changePoidsAbattage(double x){
         poidsCanard = x;
     }
+    /**
+     * Renvoie le prix du canard en multipliant sont poid par son prix au kilos
+     */
     double prix(){
         return poids * Constante.prixCanard;
     }
+    /**
+     * Renvoie True si le canard est assez gros, false sinon
+     */
     boolean assezGrosse(){
         if (poids >= poidsCanard) {
             return true;
