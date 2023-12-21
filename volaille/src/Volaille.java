d6cf0259d6b487f7df19ab57807747346ab3a98d
MrLeGalElvin <132993253+MrLeGalElvin@users.noreply.github.com>
2023-12-21 15:32:32 +0100

    Add files via upload

---
+++ volaille/src/Volaille.java
@@ -0,0 +1,17 @@
+class Volaille{
+    double poids;
+    int identite;
+    Volaille(double p, int i){
+        poids = p;
+        identite = i;
+    }
+    void changePoids(double np){
+        poids = np;
+    }
+    double prix(){
+        return 0;
+    }
+    boolean assezGrosse(){
+        return false;
+    }
+}
