58d7fa4a658d6616f141b742f4e494eb8de8d823
MrLeGalElvin <132993253+MrLeGalElvin@users.noreply.github.com>
2023-12-21 16:03:56 +0100

    !

--- volaille/src/Canard.java
+++ volaille/src/Canard.java
@@ -1,12 +1,42 @@
 public class Canard extends Volaille{
     public double poidsCanard = 1;
+    // prix du canard au kilo
+    public double prixCanard = 1;
 
     Canard(double p, int i){
         super(p,i);
     }
 
-    void changePoidsCanard(double x){
-        this.poidsCanard = x;
+    /**
+     * getter poids du canard
+     * @return poids du canard
+     */
+    public double getPoidsCanard() {
+        return poidsCanard;
+    }
+
+    /**
+     * setter poids du canard
+     * @param poidsCanard
+     */
+    public void setPoidsCanard(double poidsCanard) {
+        this.poidsCanard = poidsCanard;
+    }
+
+    /**
+     * getter prix du canard au kilo
+     * @return prix du canard au kilo
+     */
+    public double getPrixCanard() {
+        return prixCanard;
+    }
+
+    /**
+     * setter prix du canard au kilo
+     * @param prixCanard
+     */
+    public void setPrixCanard(double prixCanard) {
+        this.prixCanard = prixCanard;
     }
 
     double prix(){
