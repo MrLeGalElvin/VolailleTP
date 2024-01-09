fc9aa6133a95eb70a510ea48cb782279daf19af3
MrLeGalElvin <132993253+MrLeGalElvin@users.noreply.github.com>
2024-01-10 00:20:24 +0100

    Final

--- Rendu_TP/POO/src/main/java/fr/angers/poo/MainApp.java
+++ Rendu_TP/POO/src/main/java/fr/angers/poo/MainApp.java
@@ -10,17 +10,34 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
+/**
+ * classe principale permettant de lancer l'application
+ */
 public class MainApp extends Application {
 
+    /**
+     * Lance la methode showMenuPrincipale pour afficher la page principale
+     * @param primaryStage
+     * @throws IOException
+     */
     @Override
     public void start(Stage primaryStage) throws IOException {
         showMenuPrincipale(primaryStage);
     }
 
+    /**
+     * Lance automatiquement la methode start
+     * @param args
+     */
     public static void main(String[] args) {
         launch(args);
     }
 
+    /**
+     * Affiche la page principale récupéré dans le dossier fxml
+     * @param primaryStage
+     * @throws IOException
+     */
     public static void showMenuPrincipale(Stage primaryStage) throws IOException {
         FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("menu-principale-vue.fxml"));
         Parent root = loader.load();
